1e31312b7d3708c20c30d96144d30173a509652a
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-03 11:54:45 -0300

    percorrendo lista

--- lista.py
+++ lista.py
@@ -11,3 +11,15 @@ lista = ["p","y","t","h","o","n"]
 print(lista[0:3:2]) # inicio, final , step
 print(lista[2:]) 
 print(lista[:2]) 
+
+#percorrendo lista 
+carros = ["gol", "polo", "jeta"]
+
+for carro in carros:
+    print(f"lista de carros: {carro}")
+
+#enumerate
+carros = ["gol", "polo", "jeta"]
+
+for indice,carro in enumerate(carros):
+    print(f"{indice + 1} = {carro}")
