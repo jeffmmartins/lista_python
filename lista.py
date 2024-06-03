9f85e468c39ef2e00d1b4fa36859261a0b2b2c8b
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-03 11:50:41 -0300

    fatiamento

--- lista.py
+++ lista.py
@@ -6,3 +6,8 @@ matriz = [
 
 # primeiro valor indica a linha e segundo valor indica coluna
 print(matriz[1][1])
+
+lista = ["p","y","t","h","o","n"]
+print(lista[0:3:2]) # inicio, final , step
+print(lista[2:]) 
+print(lista[:2]) 
