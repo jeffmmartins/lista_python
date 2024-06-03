cfd090a7aa3f0e9f7373aa563ab0425591663a3d
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-03 09:48:53 -0300

    exemplo de lista

---
+++ declaracao_lista.py
@@ -0,0 +1,15 @@
+frutas = ["laranja", "banana", "maca"]
+print(frutas)
+
+# exemplo de lista vazia
+frutas = []
+print(frutas)
+
+letras = list("python")
+print(letras)
+
+numero = list(range(10))
+print(numero)
+
+carro = ["ferrari", 10, 2020, True]
+print(carro)
