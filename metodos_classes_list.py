665a4e6ac4e8ea92ddaf3c0053872a3ca5047252
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-03 15:30:50 -0300

    metodos sort, sorted

--- metodos_classes_list.py
+++ metodos_classes_list.py
@@ -15,5 +15,43 @@ print(linguagens)
 
 #index
 linguagen = ["python", "js", "c", "java", "csharp"]
-linguagen.index("java")  
-linguagen.index("python")  
+print(linguagen.index("java"))
+print(linguagen.index("python"))  
+
+#pop
+linguagens = ["python", "js", "c", "java", "csharp"]
+print(linguagens.pop())  # csharp
+print(linguagens.pop()) # java
+print(linguagens.pop())  # c
+print(linguagens.pop(0))
+
+#remove
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.remove("c")
+print(linguagens) 
+
+#reverse
+linguagens = ["python", "js", "c", "java", "csharp"] 
+linguagens.reverse()
+print(linguagens) 
+
+#sort
+linguagen = ["python", "js", "c", "java", "csharp"]
+print(linguagen.sort())  # ["c", "csharp", "java", "js", "python"]
+linguagen = ["python", "js", "c", "java", "csharp"]
+print(linguagen.sort(reverse=True))  # ["python", "js", "java", "csharp", "c"]
+linguagen = ["python", "js", "c", "java", "csharp"]
+print(linguagen.sort(key=lambda x: len(x)))  # ["c", "js", "java", "python", "csharp"]
+linguagen = ["python", "js", "c", "java", "csharp"]
+print(linguagen.sort(key=lambda x: len(x), reverse=True))
+
+#len
+linguagens = ["python", "js", "c", "java", "csharp"]
+print(len(linguagens))
+
+#sorted
+linguagens = ["python", "js", "c", "java", "csharp"]
+print(sorted(linguagens, key=lambda x: len(x))) # ["c", "js", "java", "python", 
+
+print(sorted(linguagens, key=lambda x: len(x), reverse=True)) # ["python", "csharp", "java", "js", "c"]
+
