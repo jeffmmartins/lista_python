1ec84e209ebd7c36632dbafdc725ff9a404b57ae
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-03 15:22:40 -0300

    metodos

--- metodos_classes_list.py
+++ metodos_classes_list.py
@@ -2,3 +2,18 @@ lista = [1,2,3,4]
 
 lista.clear()
 print(lista)
+
+numeros = [1,3,4,5]
+numeros.copy()
+print(numeros)
+
+
+#extend
+linguagens = ["python", "js", "c"]
+linguagens.extend(["java", "kotlin"])
+print(linguagens)
+
+#index
+linguagen = ["python", "js", "c", "java", "csharp"]
+linguagen.index("java")  
+linguagen.index("python")  
