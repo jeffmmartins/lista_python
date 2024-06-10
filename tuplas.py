d10ee6ebf383e230d9531df181dd727362f31d7e
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-10 11:31:58 -0300

    exemplos de tupla

---
+++ tuplas.py
@@ -0,0 +1,8 @@
+# em tuplas lembrar de colocar a virgula no final da lista.
+frutas = ("laranja", "maça", "banana",)
+
+print(frutas)
+
+# verifica se carrps é um tupla nesse caso irá retornar True
+carros = ("gol",)
+print(isinstance(carros, tuple))
